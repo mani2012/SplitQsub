1a63f2d414690cf34b59c4952ebff3e6e7e03afd
solaiappan manimaran <mani2012@scc4.bu.edu>
2015-06-04 14:43:53 -0400

    Adding installation scripts

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="SplitQsub",
+    version="1.0",
+    packages=find_packages(),
+    install_requires=[],
+    zip_safe=False,
+    include_package_data=True,
+    entry_points={
+        "console_scripts": [
+            "splitqsub = splitqsub.__main__:main"
+        ]
+    },
+    namespace_packages=["splitqsub"]
+    )
